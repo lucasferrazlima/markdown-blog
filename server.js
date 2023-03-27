672df36b17e40f482af9cf20664ca58d63246645
Lucas <lucasferrazl@hotmail.com>
2023-03-27 11:01:37 -0300

    Add basic setup for express server using EJS

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.listen(5000);
