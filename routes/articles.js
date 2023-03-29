8a6ed5e92170e639f7c008ebe7b96522cc1a7b01
Lucas <lucasferrazl@hotmail.com>
2023-03-29 11:51:17 -0300

    Add sanitizer for HTML using DOMPurify

--- routes/articles.js
+++ routes/articles.js
@@ -8,32 +8,47 @@ router.get('/new', (request, response) => {
   response.render('articles/new', { article });
 });
 
+router.get('/edit/:id', async (request, response) => {
+  const article = await Article.findById(request.params.id);
+  response.render('articles/edit', { article });
+});
+
 router.get('/:slug', async (request, response) => {
   const article = await Article.findOne({ slug: request.params.slug });
-  console.log(article);
   if (article == null) {
     response.redirect('/');
   }
   response.render('articles/show', { article });
 });
 
-router.post('/', async (request, response) => {
-  let article = new Article({
-    title: request.body.title,
-    description: request.body.description,
-    markdown: request.body.markdown,
-  });
-  try {
-    article = await article.save();
-    response.redirect(`articles/${article.slug}`);
-  } catch (e) {
-    response.render('articles/new', { article });
-  }
-});
+router.post('/', async (request, response, next) => {
+  request.article = new Article();
+  next();
+}, saveArticleAndRedirect('new'));
+
+router.put('/:id', async (request, response, next) => {
+  request.article = await Article.findById(request.params.id);
+  next();
+}, saveArticleAndRedirect('edit'));
 
 router.delete('/:id', async (request, response) => {
   await Article.findByIdAndDelete(request.params.id);
   response.redirect('/');
 });
+
+function saveArticleAndRedirect(path) {
+  return async (request, response) => {
+    let { article } = request;
+    article.title = request.body.title;
+    article.description = request.body.description;
+    article.markdown = request.body.markdown;
+    try {
+      article = await article.save();
+      response.redirect(`/articles/${article.id}`);
+    } catch (e) {
+      response.render(`articles/${path}`, { article });
+    }
+  };
+}
 
 module.exports = router;
